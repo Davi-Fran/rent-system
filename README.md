c8a0a04f39ff2813fe7e7527c29bc58d87e14a8f
Davi-Fran <dafaosi.avlis@gmail.com>
2026-08-03 11:24:31 -0300

    learning models

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # Anotações das Aulas de Back-End
 
 ## Aula 30/07
-### comandos DJANGO:
-- `django-admin startproject "nome_do_projeto"` -> Cria o projeto DJANGO
+### Comandos para iniciar o projeto:
+- `django-admin startproject "nome_do_projeto" .` -> Cria o projeto DJANGO
 - `django-admin startapp "nome_da_aplicacao"` -> Cria a aplicação Rest Framework
+
+## Aula 03/08
+### Comandos de migração:
+- `py manage.py makemigrations` -> Cria a migração
+- `py manage.py migrate` -> Atualiza o banco para a migração atual
--- api/models.py
+++ api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Property(models.Model):
+    title = models.CharField()
+    property_type = models.CharField(max_length=100)
+    rent_price = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.BooleanField(default=True)
+    address = models.CharField()
+    cep = models.CharField(max_length=9)
+    complement = models.CharField(max_length=100, blank=True, null=True)
+    neighborhood = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    uf = models.CharField(max_length=2)
+
+    def __str__(self):
+        return self.title
