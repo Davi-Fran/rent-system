660dd5b29260ddee0faf2da1a32c909ea8a3c269
Davi-Fran <dafaosi.avlis@gmail.com>
2026-08-06 09:43:46 -0300

    Adding models

--- README.md
+++ README.md
@@ -9,3 +9,13 @@
 ### Comandos de migração:
 - `py manage.py makemigrations` -> Cria a migração
 - `py manage.py migrate` -> Atualiza o banco para a migração atual
+
+## Aula 06/08
+### Entendendo auth_user
+Os principais campos são:
+- `is_superuser` -> Diz o usuário é o root (usuário com acesso total à aplicação) ou não
+- `is_staff` -> Diz se o usuário é administrador ou não
+- `is_active` -> Diz se o usuário está ativo ou não
+
+### Comandos
+- `py manage.py createsuperuser` -> Cria o usuário root da aplicação
--- api/models.py
+++ api/models.py
@@ -1,4 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    class UserType(models.TextChoices):
+        ADMIN = "ADMIN", "Administrator"
+        USER = "USER", "User"
+
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=20, blank=True, null=True)
+    user_type = models.CharField(
+        max_length=20,
+        choices=UserType.choices,
+        default=UserType.USER
+    )
+
+    def __str__(self):
+        return self.get_full_name() or self.username
+
 
 class Property(models.Model):
     title = models.CharField()
@@ -14,3 +32,26 @@ class Property(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Agreement(models.Model):
+    start_date = models.DateField()
+    end_date = models.DateField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.BooleanField(default=True)
+    lessor = models.ForeignKey(User, related_name="lessor")
+    renter = models.ForeignKey(User, related_name="renter")
+
+    def __str__(self):
+        return f"Agreement {self.id}"
+
+
+class Payments(models.Model):
+    payment_date = models.DateField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.BooleanField()
+    agreement = models.ForeignKey(
+        Agreement,
+        on_delete=models.CASCADE,
+        related_name="agreement"
+    )
